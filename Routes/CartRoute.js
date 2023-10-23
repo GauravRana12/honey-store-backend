d7ea88befbcbafe66896339586c0bab91f300267
Gaurav <gauravp1335@gmail.com>
2023-10-23 11:21:17 +0530

    gh

--- Routes/CartRoute.js
+++ Routes/CartRoute.js
@@ -2,8 +2,8 @@ const express=require('express');
 const bcrypt = require('bcrypt');
 const jwt=require('jsonwebtoken');
 const { Authenticate } = require('../Middlewares/Authentication');
-const productModel = require('../modals/Employee.model');
 const UserModel = require('../modals/User.model');
+const cartModel = require('../modals/CartModel');
 
 const CartRoute=express.Router();
 
@@ -12,7 +12,7 @@ CartRoute.get('/',Authenticate,async (req,res)=>{
         const id=req.userID;
         console.log(id)
         const user=await UserModel.find({_id:id});
-        const data=await productModel.find({authorID:id});
+        const data=await cartModel.find({authorID:id});
         res.send(data);
      } catch (error) {
         console.log(error);
@@ -27,7 +27,7 @@ CartRoute.post('/',Authenticate,async (req,res)=>{
             ...input,
             authorID:id
         }
-        await productModel.create(new_obj);
+        await cartModel.create(new_obj);
         res.send('Added successful');
       } catch (error) {
         console.log(error);
@@ -39,7 +39,7 @@ CartRoute.patch('/:emp_id',Authenticate,async (req,res)=>{
     try {
         const {emp_id}=req.params.emp_id;
         const input=req.body;
-        await productModel.findByIdAndUpdate(emp_id,input);
+        await cartModel.findByIdAndUpdate(emp_id,input);
         res.send("Patches successful");
     } catch (error) {
         console.log(error);
@@ -49,7 +49,7 @@ CartRoute.patch('/:emp_id',Authenticate,async (req,res)=>{
 CartRoute.delete('/:emp_id',Authenticate,async (req,res)=>{
     try {
         const {emp_id}=req.params.emp_id;
-        await productModel.findByIdAndDelete(emp_id);
+        await cartModel.findByIdAndDelete(emp_id);
         res.send('deleted successfully')
     } catch (error) {
         console.log(error);
