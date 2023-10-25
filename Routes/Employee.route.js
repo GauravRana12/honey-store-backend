8b0d3acc020b8d6bbfb1a4db55bfa8c2ee06a376
Gaurav <gauravp1335@gmail.com>
2023-10-25 08:45:06 +0530

    fgf

--- Routes/Employee.route.js
+++ Routes/Employee.route.js
@@ -38,7 +38,7 @@ EmployeeRoute.get('/',async (req,res)=>{
      }
 })
 
-EmployeeRoute.get('/:emp_id',async (req,res)=>{
+EmployeeRoute.get('/single/:emp_id',async (req,res)=>{
     try {
         const {emp_id}=req.params;
         const data=await productModel.find({_id:emp_id});
